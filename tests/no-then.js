f411f1c27a96a2a86f439ef56f33eb743590d148
Bryan Veloso <bryan@avalonstar.com>
2020-07-26 18:53:54 -0700

    chore: Add configuration based on GitHub's plugin.

---
+++ tests/no-then.js
@@ -0,0 +1,39 @@
+const rule = require('../lib/rules/no-then')
+const RuleTester = require('eslint').RuleTester
+
+const ruleTester = new RuleTester()
+
+ruleTester.run('no-then', rule, {
+  valid: [
+    {
+      code: '(async function() { const data = await read(); console.log(data) })()',
+      parserOptions: { ecmaVersion: 2017 },
+    },
+    {
+      code: '(async function() { try { await read() } catch(error) { console.error(error) } })()',
+      parserOptions: { ecmaVersion: 2017 },
+    },
+  ],
+  invalid: [
+    {
+      code: '(function() { read().then(data => console.log(data)) })()',
+      parserOptions: { ecmaVersion: 2017 },
+      errors: [
+        {
+          message: 'Prefer async/await to Promise.then()',
+          type: 'Identifier',
+        },
+      ],
+    },
+    {
+      code: '(function() { read().catch(error => console.error(error)) })()',
+      parserOptions: { ecmaVersion: 2017 },
+      errors: [
+        {
+          message: 'Prefer async/await to Promise.catch()',
+          type: 'Identifier',
+        },
+      ],
+    },
+  ],
+})
